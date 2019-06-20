94b78b592daf08af3d940c910b4118ac6e1ca4d7
Eli Sacks <bastlifa@gmail.com>
2019-06-20 16:32:42 -0400

    added animation to dropdown. It looks awful

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -17,10 +17,24 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle("dropdown-hidden");
+    // this.content.classList.toggle("dropdown-hidden");
+
+    if (Array.from(this.content.classList).includes("dropdown-hidden"))
+    {
+      TweenMax.to(this.content, .5,
+      {
+        className:"-=dropdown-hidden"
+      })
+    }
+    else
+    {
+      TweenMax.to(this.content, .5, 
+      {
+        className:"+=dropdown-hidden"
+      });
+    }
   }
 }
-
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
